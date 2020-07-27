7e6b615883c79f778a4bccda5dde0f866e5c99ec
Mathursan-007 <63546506+Mathursan-007@users.noreply.github.com>
2020-07-27 18:33:42 +0530

    Commit after 1st merge

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.myfirstapp;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,15 +22,49 @@ public class MainActivity extends AppCompatActivity {
 
     public void sendmessage(View view){
 
+        String msg="Validating";
+        Toast toast=Toast.makeText(this.getApplicationContext(),msg,Toast.LENGTH_SHORT);
+        toast.show();
+
         Intent intent=new Intent(this,DisplayMessageActivity.class);
+        final Intent intent2=new Intent(this,MainActivity.class);
         EditText editText=findViewById(R.id.editTextTextPersonName);
         EditText editText1=findViewById(R.id.editTextTextPassword);
         String message=editText.getText().toString();
         String pwd=editText1.getText().toString();
 
+
         if(message.equalsIgnoreCase("Admin")&&pwd.equalsIgnoreCase("Admin")){
 
+            Toast.makeText(MainActivity.this, "Success", Toast.LENGTH_SHORT).show();
             startActivity(intent);
+        }else{
+
+            AlertDialog.Builder builder =new AlertDialog.Builder(this);
+            builder.setMessage(R.string.dialog_message);
+
+
+            builder.setNeutralButton("Ok", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    startActivity(intent2);
+                }
+            });
+
+
+            builder.setPositiveButton("Retry", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+
+                    startActivity(intent2);
+
+                }
+            });
+
+           
+            AlertDialog alertDialog=builder.create();
+            alertDialog.show();
+
         }
 
             //Comment written by Arosh
